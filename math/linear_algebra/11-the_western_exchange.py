54f882d6700bffd71f7beafa7bfd0ebc6ece52b3
Jehp00 <jehp01104006@gmail.com>
2023-03-21 23:01:52 -0500

    transpose

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module the western_exchange\math"""
+
+
+def np_transpose(matrix):
+    """
+    :param matrix: matrix to transpose
+    :return: the matrix trnsposed
+    """
+    return matrix.transpose()
