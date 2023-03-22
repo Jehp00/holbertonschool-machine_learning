9c4213ca4060198e4f1edf79c1228f363db63047
Jehp00 <jehp01104006@gmail.com>
2023-03-21 22:45:32 -0500

    shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module ill use my scale"""
+import numpy
+
+
+def np_shape(matrix):
+    """
+    :param matrix: matrix to get the shape
+    :return: the shape of the matrix
+    """
+    return matrix.shape
