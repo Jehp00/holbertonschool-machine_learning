a4cb72efb6a0e8d08b721316b5ed1577fc937ca6
Jehp00 <jehp01104006@gmail.com>
2023-03-21 23:31:48 -0500

    matrix multiplicate

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module saddle up"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    :param mat1: matrix
+    :param mat2: matrix
+    :return: performs matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
