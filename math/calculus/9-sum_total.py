e7ad4f4c0f59df754f60411f1d701fd90d9bbaec
Jehp00 <jehp01104006@gmail.com>
2023-03-31 00:32:44 -0500

    sigma sum in python

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """This function calculates the sum of the numbers squared
     until n"""
-    if isinstance(n, int) and isinstance(n, float):
-        return int((n * (n + 1) * (2 * n + 1)) / 6)
-    return None
+    if not isinstance(n, int) and not isinstance(n, float):
+        return None
+    return int((n * (n + 1) * (2 * n + 1)) / 6)
