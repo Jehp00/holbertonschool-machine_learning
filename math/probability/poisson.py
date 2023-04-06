04e14dfeb9bbba047af32e75d19eb2f063f6d7f8
Jehp00 <jehp01104006@gmail.com>
2023-04-06 18:33:06 -0500

    probability possion

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""Module poisson class"""
+"""This module contains the class Poisson"""
 
 
-class Possion:
-    """
-    Poisson Distribution
-    """
-    def __int__(self, data=None, lambtha=1.):
+class Poisson:
+    """This class represents a poisson distribution"""
+    def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -17,12 +15,14 @@ class Possion:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data)/len(data))
 
     def pmf(self, k):
-        """PMF of a given number of success"""
+        """Calculates the value of the PMF for a given number of successes"""
         e = 2.7182818285
         if type(k) is not int:
             k = int(k)
+
         if k < 0:
             return 0
 
@@ -33,10 +33,11 @@ class Possion:
         return ((e**(-self.lambtha))*(self.lambtha**k))/fact
 
     def cdf(self, k):
-        """CDF for a given number of success"""
+        """Calculates the value of the CDF for a given number of successes"""
         e = 2.7182818285
         if type(k) is not int:
             k = int(k)
+
         if k < 0:
             return 0
 
@@ -45,5 +46,5 @@ class Possion:
             fact = 1
             for i in range(1, i+1):
                 fact = fact * i
-            res += ((e**(-self.lambtha))*(self.lambtha**i)/fact
+            res += ((e**(-self.lambtha))*(self.lambtha**i))/fact
         return res
