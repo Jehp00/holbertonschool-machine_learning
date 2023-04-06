cf56b265019432126ee1c1c3f7937908b76052dd
Jehp00 <jehp01104006@gmail.com>
2023-04-06 18:47:48 -0500

    exponential probability

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -15,7 +15,7 @@ class Exponential:
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(data) < 0:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = 1 / float(sum(data) / len(data))
