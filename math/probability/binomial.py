10d990fa5c0e0a2dcb716d2c88bb8b92166f7a27
Jehp00 <jehp01104006@gmail.com>
2023-04-06 19:42:33 -0500

    binominal probability

---
+++ math/probability/binomial.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env pyhton3
+"""Module binomial"""
+
+
+class Binomial:
+    """Binomial Distribution"""
+    def __init__(self, data=None, n=1, p=0.5):
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            else:
+                self.n = int(n)
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            else:
+                self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            x_bar = sum(data) / len(data)
+            s2 = sum((x - x_bar)**2 for x in data) / len(data)
+            self.n = round((x_bar ** 2) / (x_bar - s2))
+            self.p = x_bar / self.n
+
+    def pmf(self, k):
+        """
+        :param k: number of success
+        :return: PMF of k
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        def fact(x):
+            fact = 1
+            for i in range(1, x + 1):
+                fact = fact * i
+            return fact
+        combinatoric = (fact(self.n)) / (fact(k) * fact(self.n - k))
+        return combinatoric * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """
+        :param k: number of success
+        :return: cdf of a given k
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        return sum([self.pmf(x) for x in range(k + 1)])
