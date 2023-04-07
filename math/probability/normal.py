1b65089fa10834a1b482ed795da70c1daac29a36
Jehp00 <jehp01104006@gmail.com>
2023-04-06 19:14:18 -0500

    normal probability

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -17,7 +17,8 @@ class Normal:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.mean = float(sum(data) / len(data))
-            self.stddev = (sum((x - (sum(data) / len(data))) ** 2 for x in data) / len(data))**0.5
+            self.stddev = (sum((x - (sum(data) / len(data))) **
+                           2 for x in data) / len(data))**0.5
 
     def z_score(self, x):
         """
