0f43b1be85cb96d00e3c9ba8ba8b67634cb0cb14
Jehp00 <jehp01104006@gmail.com>
2023-04-14 15:41:50 -0500

    forward propagation

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""module first neuron"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Class Neuron
+    """
+
+    def __init__(self, nx):
+        """
+        :param nx: Number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0, 1, size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        :param X: numpy.ndarray with shape (nx, m) that contains
+        the input data
+        :return: the forward propagation of the neuron
+        """
+        z = np.dot(self.W, X) + self.b
+        self.__A = 1/(1 + np.exp(-z))
+        return self.__A
