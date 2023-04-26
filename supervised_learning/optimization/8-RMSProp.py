8323230ac570ab4d49adc44ebfb9f058552cce03
Jehp00 <jehp01104006@gmail.com>
2023-04-26 02:26:47 -0500

    RMSPropOptimizer

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -2,6 +2,7 @@
 """module  optimization"""
 import tensorflow as tf
 
+
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """Creates the training operation for a neural network in TensorFlow
         using the RMSProp optimization algorithm"""
