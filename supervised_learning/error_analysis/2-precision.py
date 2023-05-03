3a331f1f073a31ca84a1f28e881258365834927e
Jehp00 <jehp01104006@gmail.com>
2023-05-02 23:03:40 -0500

    precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module error analysis"""
+import numpy as np
+
+
+def precision(confusion):
+    """Calculates the precision for each class in a
+    confusion matrix"""
+    classes = confusion.shape[0]
+    precision = []
+    for c in range(classes):
+        correct = 0
+        totally = 0
+        for r in range(classes):
+            if r == c:
+                correct += confusion[r][c]
+            totally += confusion[r][c]
+        precision.append(confusion / totally)
+    return np.asarray(precision)
