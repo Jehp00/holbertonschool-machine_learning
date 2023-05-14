7b0a36d2c26217ba8774f764967ac0bcb80a4a51
Jehp00 <jehp01104006@gmail.com>
2023-05-14 17:20:52 -0500

    model sequential

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -7,15 +7,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
     L2 = K.regularizers.l2(lambtha)
     model = K.Sequential()
-    model.add(K.layers..Dense(layers[0], input_shape=(nx,),
-                              activation=activations[0],
-                              kernel_regularizer=L2,
-                              name='dense'))
+    model.add(K.layers.Dense(layers[0], input_shape=(nx,),
+                             activation=activations[0],
+                             kernel_regularizer=L2,
+                             name='dense'))
 
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[i],
-                                 activation=activations[0],
+                                 activation=activations[i],
                                  kernel_regularizer=L2,
                                  name='dense_' + str(i)))
 
