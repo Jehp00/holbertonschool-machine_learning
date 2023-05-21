d91ec849a72941c3704b8e400898cf12ccec405b
Jehp00 <jehp01104006@gmail.com>
2023-05-21 16:55:24 -0500

    task 1

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -2,6 +2,7 @@
 """Module convolutional neurak netwwork"""
 import numpy as np
 
+
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     performs forward propagation over a pooling layer of a neural network
