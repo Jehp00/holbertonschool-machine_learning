889c401c6b0f446b688395ff13d737a6f832fdad
Jehp00 <jehp01104006@gmail.com>
2023-06-18 20:59:28 -0500

    tasks 0 and 1

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""This model contains the class Yolo"""
+"""This module contains the class Yolo"""
 import tensorflow.keras as K
 
 
 class Yolo():
     """uses the Yolo v3 algorithm to perform object detection"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
         :param model_path: the path to where a Darknet Keras model is stored
@@ -23,9 +24,9 @@ class Yolo():
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             lines = f.readlines()
-            self.class_name = []
+            self.class_names = []
             for name in lines:
-                self.class_name.append(name[:-1])
+                self.class_names.append(name[:-1])
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
