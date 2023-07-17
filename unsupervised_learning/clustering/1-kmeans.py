019c8f96de6c0c7204e5b88b8a650d9e0b3f1391
Jehp00 <jehp01104006@gmail.com>
2023-07-17 17:32:29 -0500

    task 0, 1, 2

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -14,32 +14,38 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    N, D = X.shape
-
+        # Setting min and max values per col
+    n, d = X.shape
     X_min = X.min(axis=0)
     X_max = X.max(axis=0)
 
-    C = np.random.uniform(X_min, X_max, (k, D))
+    # Centroid
+    C = np.random.uniform(X_min, X_max, size=(k, d))
 
+    # Loop per the maximum number of iterations
     for i in range(iterations):
-        cdois = np.copy(C)
-        cdois_entended = C[:, np.newaxis]
 
-        distance = np.sqrt(((X - cdois_entended) ** 2).sum(axis=2))
+        # initializes k centroids by selecting them from the data points
+        centroids = np.copy(C)
+        centroids_extended = C[:, np.newaxis]
 
-        clss = np.argmin(distance, axis=0)
+        # distances also know as euclidean distance
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
+        # an array containing the index to the nearest centroid per each point
+        clss = np.argmin(distances, axis=0)
 
-        for cl in range(k):
-            if X[clss == cl].size == 0:
-                C[cl] = np.random.uniform(X_min, X_max, size=(1, D))
+        # Assign all points to the nearest centroid
+        for c in range(k):
+            if X[clss == c].size == 0:
+                C[c] = np.random.uniform(X_min, X_max, size=(1, d))
             else:
-                C[cl] = X[clss == cl].mean(axis=0)
+                C[c] = X[clss == c].mean(axis=0)
 
-        cdois_entended = C[:, np.newaxis]
-        distance = np.sqrt(((X - cdois_entended) ** 2).sum(axis=2))
-        clss =np.argmin(distance, axis=0)
+        centroids_extended = C[:, np.newaxis]
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
+        clss = np.argmin(distances, axis=0)
 
-        if (cdois == C).all():
+        if (centroids == C).all():
             break
 
-        return C, clss
+    return C, clss
